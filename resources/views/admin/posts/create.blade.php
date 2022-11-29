4a37e565b6402488f96a81efe2471234ff0a8463
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-11-29 20:13:30 +0800

    完成create.blade.php當中的<form>表單

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,7 +15,7 @@
         </ul>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-    <form action="{{ route('admin.posts.store') }}" method="post">
+    <!--<form action="{{ route('admin.posts.store') }}" method="post">
         @method('POST')
         @csrf
         <div class="mb-3">
@@ -28,6 +28,25 @@
         </div>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <button class="btn btn-primary btn-sm" type="submit">儲存</button>
+        </div>
+    </form>-->
+    <form action="/admin/posts" method="POST" role="form">
+        @method('POST')
+        @csrf
+        <div class="form-group">
+            <label for="title" class="form-label">標題：</label>
+            <input id="title" name="title" class="form-control" value="{{old('title')}}" placeholder="請輸入文章標題">
+            <label for="content" class="form-label>內容：</label>
+            <textarea id="content" name="content" class="form-control" rows="10">{{old('content')}}</textarea>
+        </div>
+        <div class="form-group">
+            <label for=" is_feature " class="form-label">精選？</label>
+            <select id=" is_feature " name="is_feature" class="form-control">
+                <option value="0">否</option>
+                <option value="1">是</option>
+            </select>
+        </div>
+        <div class="text-right">
         </div>
     </form>
 </div>
